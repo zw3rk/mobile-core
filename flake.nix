2f4326b4861a1b2bb56182f7b7110a5bb9d8d53b
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-17 18:59:21 +0800

    ignore result

---
+++ flake.nix
@@ -0,0 +1,24 @@
+let systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+in {
+  description = "Package build for lastpass-tui";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, haskellNix, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem systems (system:
+      let pkgs = haskellNix.legacyPackages.${system}; in
+      let drv = pkgs.haskell-nix.project {
+        compiler-nix-name = "ghc8107";
+        index-state = "2020-12-15T00:00:00Z";
+        src = pkgs.haskell-nix.haskellLib.cleanGit {
+          name = "mobile-core";
+          src = ./.;
+        };
+      }; in
+      rec {
+        packages = {
+          "lib:mobile-core" = drv.mobile-core.components.library;
+        };
+      }
+    );
+}
