08cd14d97e6aa1a940b1b2a9d7981236cac784c2
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-17 21:48:36 +0800

    Full flake multiplexing

--- flake.nix
+++ flake.nix
@@ -4,10 +4,15 @@
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix/angerman/aarch64";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   outputs = { self, haskellNix, nixpkgs, flake-utils }:
-    let systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ]; in
+    let
+        ghcjs = { config = "js-unknown-ghcjs"; };
+        x86_64-musl64 = { config = "x86_64-unknown-linux-musl"; };
+        aarch64-musl64 = { config = "aarch64-unknown-linux-musl"; };
+        systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    in
     flake-utils.lib.eachSystem systems (system:
       let pkgs = haskellNix.legacyPackages.${system}; in
-      let drv = pkgs.haskell-nix.project {
+      let drv = pkgs': pkgs'.haskell-nix.project {
         compiler-nix-name = "ghc8107";
         index-state = "2020-12-15T00:00:00Z";
         src = pkgs.haskell-nix.haskellLib.cleanGit {
@@ -17,8 +22,15 @@
       }; in
       rec {
         packages = {
-          "lib:mobile-core" = drv.mobile-core.components.library;
-        };
+          "lib:mobile-core" = (drv pkgs).mobile-core.components.library;
+        } // ({ "x86_64-linux" = {
+                    "ghcjs:lib:mobile-core" = (drv (haskellNix.internal.compat { inherit system; crossSystem = ghcjs; }).pkgs).mobile-core.components.library;
+                    "musl64:lib:mobile-core" = (drv (haskellNix.internal.compat { inherit system; crossSystem = x86_64-musl64; }).pkgs).mobile-core.components.library;
+                };
+                "aarch64-linux" = {
+                    "musl64:lib:mobile-core" = (drv (haskellNix.internal.compat { inherit system; crossSystem = aarch64-musl64; }).pkgs).mobile-core.components.library;
+                };
+            }.${system} or {});
         # build all packages in hydra.
         hydraJobs = packages;
       }
