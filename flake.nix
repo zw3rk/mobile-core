303d54b0a38722f431db15f111a6d0120179c2a2
Moritz Angermann <moritz.angermann@gmail.com>
2021-12-22 11:30:28 +0800

    bump

--- flake.nix
+++ flake.nix
@@ -56,13 +56,72 @@
           "exe:mobile-core:mobile-core-c" = (drv pkgs).mobile-core.components.exes.mobile-core-c;
           "lib:ffi:static" = pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; });
           "lib:gmp:static" = pkgs.gmp6.override { withStatic = true; };
-        } // ({ "x86_64-linux" = let muslPkgs = pkgs.pkgsCross.musl64; in {
+        } // ({ "x86_64-linux" = let muslPkgs = pkgs.pkgsCross.musl64; androidPkgs = pkgs.pkgsCross.aarch64-android; in {
                     "ghcjs:lib:mobile-core" = (drv pkgs.pkgsCross.ghcjs).mobile-core.components.library;
+
                     "musl64:lib:ffi:static" = muslPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; });
                     "musl64:lib:gmp:static" = muslPkgs.gmp6.override { withStatic = true; };
                     "musl64:lib:mobile-core" = (drv muslPkgs).mobile-core.components.library;
                     "musl64:exe:mobile-core:mobile-core" = (drv muslPkgs).mobile-core.components.exes.mobile-core;
                     "musl64:exe:mobile-core:mobile-core-c" = (drv muslPkgs).mobile-core.components.exes.mobile-core-c;
+                    "musl64:lib:mobile-core:smallAddressSpace" = (drv muslPkgs).mobile-core.components.library.override {
+                      smallAddressSpace = true; enableShared = false;
+                      ghcOptions = [ "-staticlib" ];
+                      postInstall = ''
+                        ${pkgs.tree}/bin/tree $out
+                        mkdir -p $out/_pkg
+                        # copy over includes, we might want those, but maybe not.
+                        cp -r $out/lib/*/*/include $out/_pkg/
+                        # find the libHS...ghc-X.Y.Z.a static library; this is the
+                        # rolled up one with all dependencies included.
+                        find ./dist -name "libHS*-ghc*.a" -exec cp {} $out/_pkg \;
+
+                        find ${muslPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        find ${muslPkgs.gmp6.override { withStatic = true; }}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        find ${muslPkgs.stdenv.cc.libc}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        
+                        ${pkgs.tree}/bin/tree $out/_pkg
+                        (cd $out/_pkg; ${pkgs.zip}/bin/zip -r -9 $out/pkg.zip *)
+                        rm -fR $out/_pkg
+
+                        mkdir -p $out/nix-support
+                        echo "file binary-dist \"$(echo $out/*.zip)\"" \
+                           > $out/nix-support/hydra-build-products
+                      '';
+                    };
+
+
+                    "aarch64-android:lib:ffi:static" = androidPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; });
+                    "aarch64-android:lib:gmp:static" = androidPkgs.gmp6.override { withStatic = true; };
+                    "aarch64-android:lib:mobile-core" = (drv androidPkgs).mobile-core.components.library;
+                    "aarch64-android:exe:mobile-core:mobile-core" = (drv androidPkgs).mobile-core.components.exes.mobile-core;
+                    "aarch64-android:exe:mobile-core:mobile-core-c" = (drv androidPkgs).mobile-core.components.exes.mobile-core-c;
+                    "aarch64-android:lib:mobile-core:smallAddressSpace" = (drv androidPkgs).mobile-core.components.library.override {
+                      smallAddressSpace = true; enableShared = false;
+                      ghcOptions = [ "-staticlib" ];
+                      postInstall = ''
+                        ${pkgs.tree}/bin/tree $out
+                        mkdir -p $out/_pkg
+                        # copy over includes, we might want those, but maybe not.
+                        cp -r $out/lib/*/*/include $out/_pkg/
+                        # find the libHS...ghc-X.Y.Z.a static library; this is the
+                        # rolled up one with all dependencies included.
+                        find ./dist -name "libHS*-ghc*.a" -exec cp {} $out/_pkg \;
+
+                        find ${androidPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        find ${androidPkgs.gmp6.override { withStatic = true; }}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        find ${androidPkgs.stdenv.cc.libc}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        
+                        ${pkgs.tree}/bin/tree $out/_pkg
+                        (cd $out/_pkg; ${pkgs.zip}/bin/zip -r -9 $out/pkg.zip *)
+                        rm -fR $out/_pkg
+
+                        mkdir -p $out/nix-support
+                        echo "file binary-dist \"$(echo $out/*.zip)\"" \
+                           > $out/nix-support/hydra-build-products
+                      '';
+                    };
+
                 };
                 "aarch64-linux" = let muslPkgs = pkgs.pkgsCross.aarch64-multiplatform-musl; androidPkgs = pkgs.pkgsCross.aarch64-android; in {
                     "musl64:lib:ffi:static" = muslPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; });
@@ -84,7 +143,7 @@
 
                         find ${muslPkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib -name "*.a" -exec cp {} $out/_pkg \;
                         find ${muslPkgs.gmp6.override { withStatic = true; }}/lib -name "*.a" -exec cp {} $out/_pkg \;
-                        find ${muslPkgs.musl}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        find ${muslPkgs.stdenv.cc.libc}/lib -name "*.a" -exec cp {} $out/_pkg \;
                         
                         ${pkgs.tree}/bin/tree $out/_pkg
                         (cd $out/_pkg; ${pkgs.zip}/bin/zip -r -9 $out/pkg.zip *)
@@ -140,6 +199,7 @@
 
                         find ${pkgs.libffi.overrideAttrs (old: { dontDisableStatic = true; })}/lib -name "*.a" -exec cp {} $out/_pkg \;
                         find ${pkgs.gmp6.override { withStatic = true; }}/lib -name "*.a" -exec cp {} $out/_pkg \;
+                        # There is no static libc 
 
                         ${pkgs.tree}/bin/tree $out/_pkg
                         (cd $out/_pkg; ${pkgs.zip}/bin/zip -r -9 $out/pkg.zip *)
